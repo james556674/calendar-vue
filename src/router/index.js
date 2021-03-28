fd71a825a2b0857a4604d24253331470973f0887
James <james556674@gmail.com>
2021-03-28 15:16:01 +0800

    feat: project init

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '*',
+    name: 'Not-Found',
+    component: () => import('../views/NotFound.vue')
+  }
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>this is a calendar</h1>
+</template>
